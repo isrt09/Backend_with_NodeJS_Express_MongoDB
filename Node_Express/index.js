6777af849819d113a6cba0160aed89c6aba842d4
isrt09 <mazedur@gmail.com>
2021-04-18 13:59:28 +0600

    Response Methods

--- Node_Express/index.js
+++ Node_Express/index.js
@@ -4,6 +4,15 @@ const app = express();
 app.get('/', function(req, res){
     res.send('Hello Express JS');
 });
+app.post('/about', function(req, res){
+    res.send('Hello About Page');
+});
+app.put('/blog', function(req, res){
+    res.send('Hello Blog Page');
+});
+app.delete('/contact', function(req, res){
+    res.send('Hello Contact Page');
+});
 
 app.listen(7000, function(){
     console.log('Server run success...');
