72d62bd52d6cda5e3d11e6267196551b3f84b3ed
isrt09 <mazedur@gmail.com>
2021-04-19 15:02:23 +0600

    Simple REST API using Postman

--- Express/app.js
+++ Express/app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app     = express();
 
+// HTTP Methods using POSTMAN
+// GET, POST, PUT, DELETE
+
 app.get('/', (req,res)=>{
-    res.send('Welcome to Express JS');
+    res.send('I am a GET request at HOME Route');
+})
+
+app.post('/', (req,res)=>{
+    res.send('I am a POST request at HOME Route');
+})
+
+app.put('/', (req,res)=>{
+    res.send('I am a PUT request at HOME Route');
+})
+
+app.delete('/', (req,res)=>{
+    res.send('I am a DELETE request at HOME Route');
 })
 
 
