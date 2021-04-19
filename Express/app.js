bffac916c7a47c50c358c1f8cfe7693f39e8c8dc
isrt09 <mazedur@gmail.com>
2021-04-19 14:48:38 +0600

    Server initial & Express setup

---
+++ Express/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app     = express();
+
+app.get('/', (req,res)=>{
+    res.send('Welcome to Express JS');
+})
+
+
+
+module.exports = app;
--- Node_Express/index.js
+++ Node_Express/index.js
@@ -1,59 +1,26 @@
-const express = require('express')
-const app = express();
+var  express  = require('express');
+var  app      = express();
 
-// Response get Method
-app.get('/', function(req, res){
-    res.send('Hello Express JS');
-});
-
-// Response post Method
-app.post('/about', function(req, res){
-    res.send('Hello About Page');
-});
-// Response put Method
-app.put('/blog', function(req, res){
-    res.send('Hello Blog Page');
-});
-
-// Response delete Method
-app.delete('/contact', function(req, res){
-    res.send('Hello Contact Page');
-});
-
-// String Response
-app.get('/career', function(req, res){
-    res.end('This is demo text version ...');
-});
-
-// Status Response
-app.get('/service', function(req,res){
-    res.status(401).end('Unauthorized user');
+app.use(function(req, res, middle){
+    console.log('I am middle');
+    middle();
 })
 
-// JSON Response
-app.get('/json', function(req,res){
-    let data =  [
-        {
-            name : 'IBCS-PRIMAX',
-            age  :  '35',
-            dob  :  '20-11-1985'
-        },
-        {
-            name : 'IBCS-PRIMAX',
-            age  :  '55',
-            dob  :  '10-11-1985'
-        },
-        {
-            name : 'IBCS-PRIMAX',
-            age  :  '45',
-            dob  :  '23-11-1985'
-        },
-    ]
-    res.json(data);
+// Midlleware Workflow ...
+app.get('/', function(req, res, middle){
+    res.send('Home Page');
 })
 
-// Port Setup
+app.get('/home', function(req, res){
+    res.send('Hello Nodemon for Express');
+})
+
+// URL Port Setup
 app.listen(7000, function(){
     console.log('Server run success...');
 })
+
+
+
+
 
