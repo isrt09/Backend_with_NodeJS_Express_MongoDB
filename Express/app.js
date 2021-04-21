ecc5d407a110204aa354e0b79be0c598e3d0ec03
isrt09 <mazedur@gmail.com>
2021-04-21 19:06:40 +0600

    HTTP Response, JSON, File, Status Code

--- Express/app.js
+++ Express/app.js
@@ -3,10 +3,27 @@ const app        = express();
 const userRoute  = require('./routes/users.route');
 
 app.use("/api/user/",userRoute);
+
+app.use("/login", (req,res)=>{
+    res.send('Hi, I am login Page');
+});
+
+app.use("/register", (req,res)=>{
+    res.send("Hi, I am Register Page");
+}) 
+
+app.use("/json", (req, res)=>{
+    // res.status(200).json({
+    //     name       :'Mazedur Rahman',
+    //     statusCode : 200
+    // });
+    res.redirect('/login');
+})
 // Express Default Routes
 
 app.get('/', (req,res)=>{
-    res.send('I am a GET request at HOME Route');
+    //res.send('I am a GET request at HOME Route');
+    res.sendFile(__dirname+"/views/index.html")
 })
 
 app.use((req, res)=>{
